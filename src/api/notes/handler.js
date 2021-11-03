bf969c112af5bc5c8484917deda1c28641b09b98
MimiPA <paramitaaditung@yahoo.com>
2021-11-04 00:20:42 +0800

    Postgres database implemented

--- src/api/notes/handler.js
+++ src/api/notes/handler.js
@@ -12,12 +12,12 @@ class NotesHandler {
         this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this);
     }
 
-    postNoteHandler(request, h) {
+    async postNoteHandler(request, h) {
         try {
             this._validator.validateNotePayload(request.payload);
             const { title = 'untitled', tags, body } = request.payload;
 
-            const noteId = this._service.addNote({ title, body, tags });
+            const noteId = await this._service.addNote({ title, body, tags });
 
             const response = h.response({
                 status: 'success',
@@ -49,8 +49,8 @@ class NotesHandler {
         }
     }
 
-    getNotesHandler() {
-        const notes = this._service.getNotes();
+    async getNotesHandler() {
+        const notes = await this._service.getNotes();
 
         return {
             status: 'success',
@@ -60,11 +60,11 @@ class NotesHandler {
         };
     }
 
-    getNoteByIdHandler(request, h) {
+    async getNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
 
-            const note = this._service.getNoteById(id);
+            const note = await this._service.getNoteById(id);
 
             return {
                 status: 'success',
@@ -93,12 +93,12 @@ class NotesHandler {
         }
     }
 
-    putNoteByIdHandler(request, h) {
+    async putNoteByIdHandler(request, h) {
         try {
             this._validator.validateNotePayload(request.payload);
             const { id } = request.params;
 
-            this._service.editNoteById(id, request.payload);
+            await this._service.editNoteById(id, request.payload);
 
             return {
                 status: 'success',
@@ -125,11 +125,11 @@ class NotesHandler {
         }
     }
 
-    deleteNoteByIdHandler(request, h) {
+    async deleteNoteByIdHandler(request, h) {
         try {
             const { id } = request.params;
 
-            this._service.deleteNoteById(id);
+            await this._service.deleteNoteById(id);
 
             return {
                 status: 'success',
