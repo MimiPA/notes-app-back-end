b899e9cd3994f33190b80e3876e6a5451ad544c4
Paramita <paramitaaditung@yahoo.com>
2022-02-06 17:59:34 +0800

    authentication implemented

--- src/server.js
+++ src/server.js
@@ -1,20 +1,29 @@
+// mengimpor dotenv dan menjalankan konfigurasinya
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+const Jwt = require('@hapi/jwt');
 
-//notes
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
-//users
+// users
 const users = require('./api/users');
 const UsersService = require('./services/postgres/UsersService');
 const UsersValidator = require('./validator/users');
 
+// authentications
+const authentications = require('./api/authentications');
+const AuthenticationsService = require('./services/postgres/AuthenticationsService');
+const TokenManager = require('./tokenize/TokenManager');
+const AuthenticationsValidator = require('./validator/authentications');
+
 const init = async () => {
     const notesService = new NotesService();
     const usersService = new UsersService();
+    const authenticationsService = new AuthenticationsService();
 
     const server = Hapi.server({
         port: process.env.PORT,
@@ -24,6 +33,30 @@ const init = async () => {
                 origin: ['*'],
             },
         },
+    });
+
+    // registrasi plugin eksternal
+    await server.register([
+        {
+            plugin: Jwt,
+        },
+    ]);
+
+    // mendefinisikan strategy autentikasi jwt
+    server.auth.strategy('notesapp_jwt', 'jwt', {
+        keys: process.env.ACCESS_TOKEN_KEY,
+        verify: {
+            aud: false,
+            iss: false,
+            sub: false,
+            maxAgeSec: process.env.ACCESS_TOKEN_AGE,
+        },
+        validate: (artifacts) => ({
+            isValid: true,
+            credentials: {
+                id: artifacts.decoded.payload.id,
+            },
+        }),
     });
 
     await server.register([
@@ -39,6 +72,15 @@ const init = async () => {
             options: {
                 service: usersService,
                 validator: UsersValidator,
+            },
+        },
+        {
+            plugin: authentications,
+            options: {
+                authenticationsService,
+                usersService,
+                tokenManager: TokenManager,
+                validator: AuthenticationsValidator,
             },
         },
     ]);
--- src/services/postgres/UsersService.js
+++ src/services/postgres/UsersService.js
@@ -3,10 +3,34 @@ const { Pool } = require('pg');
 const bcrypt = require('bcrypt');
 const InvariantError = require('../../exceptions/InvariantError');
 const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthenticationError = require('../../exceptions/AuthenticationError');
 
 class UsersService {
     constructor() {
         this._pool = new Pool();
+    }
+
+    async verifyUserCredential(username, password) {
+        const query = {
+            text: 'SELECT id, password FROM users WHERE username = $1',
+            values: [username],
+        };
+
+        const result = await this._pool.query(query);
+
+        if (!result.rows.length) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        const { id, password: hashedPassword } = result.rows[0];
+
+        const match = await bcrypt.compare(password, hashedPassword);
+
+        if (!match) {
+            throw new AuthenticationError('Kredensial yang Anda berikan salah');
+        }
+
+        return id;
     }
 
     async addUser({ username, password, fullname }) {
